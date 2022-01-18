3e05dbc0d2f869eeff5da8b988ca6d76e8ee8cf5
Courtney Huang <chuang31@stuy.edu>
2022-01-18 13:21:07 -0500

    fix this pls

---
+++ v0/Customer.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Customer {
+  double budget = Math.round((Math.random() * 50.00) * 100)/100.0;
+
+  public void play() {
+    System.out.println("Here is your budget, use it wisely: $" + budget);
+    System.out.println(menu);
+  }
+
+
+    String [][] menu = new String[10][1]){
+    //String menu = "Menu: ";
+    menu["Latte"]["$6.00"];
+
+
+}
+
+
+
+}
