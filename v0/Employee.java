a1da03a0815bef9d37f1bc60cef01dde5a2f75c8
yukifengg <yuki.2102005@gmail.com>
2022-01-18 13:19:19 -0500

    Employee

--- v0/Employee.java
+++ v0/Employee.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Employee {
-
-  public static void eplay() {
+  public void eplay() {
     Scanner e = new Scanner(System.in);
     System.out.println("What should I call you?");
     String eName = e.nextLine();
-    System.out.println("Hello, " + eName + ". Your hourly wage is $15 per hour. You work 3 days of the week with 8 hour shifts. You get paid biweekly.");
+    System.out.println("Hello, " + eName + "! Your hourly wage is $15 per hour. You work 3 days of the week with 8 hour shifts. You get paid biweekly. Please memorize our menu.");
+    String [][] menu = new String [10][1];
   }
 }
