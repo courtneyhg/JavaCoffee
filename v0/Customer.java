0e0547a081c8e799ef7a001931634ff604010d5c
courtneyhg <chuang31@stuy.edu>
2022-01-18 19:51:46 -0500

    added dashes for flair

--- v0/Customer.java
+++ v0/Customer.java
@@ -12,22 +12,22 @@ public class Customer {
     int item = 6;
     int prices = 2;
     String[][] newArray = new String[item][prices];
-    newArray[0][0] = "6 oz of cat poop coffee ";
+    newArray[0][0] = "6 oz of cat poop coffee - ";
     newArray[0][1] = "$35.25";
 
-    newArray[1][0] = "8 oz of cat poop coffee ";
+    newArray[1][0] = "8 oz of cat poop coffee - ";
     newArray[1][1] = "$45.50";
 
-    newArray[2][0] = "regular coffee ";
+    newArray[2][0] = "regular coffee - ";
     newArray[2][1] = "$3.00";
 
-    newArray[3][0] = "macchiato ";
+    newArray[3][0] = "macchiato - ";
     newArray[3][1] = "$3.50";
 
-    newArray[4][0] = "flat white ";
+    newArray[4][0] = "flat white - ";
     newArray[4][1] = "$4.25";
 
-    newArray[5][0] = "cuppuccino ";
+    newArray[5][0] = "cuppuccino - ";
     newArray[5][1] = "$5.50";
 
     for(int i = 0; i < newArray.length; i++){
@@ -38,5 +38,5 @@ public class Customer {
     }
 
   }
-  
+
 }
