a42c114796a769115cec9603045542e147e0e58e
Yuki Feng <yuki.2102005@gmail.com>
2022-01-20 11:32:01 -0500

    update java files

---
+++ v3/Woo.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Woo {
+
+  public static void main(String[] args) throws InterruptedException {
+    Scanner g = new Scanner(System.in);
+    // Woo back = new Woo();
+    Customer bob = new Customer();
+    Employee joe = new Employee();
+    System.out.println("\nLaunching Game...");
+    //commenting out animationjust to save time for now whenever we run to fix errors
+    // int l = 0;
+    // while (l < 25) {
+    //   System.out.print(" - ");
+    //   Thread.sleep(100);
+    //   l++;
+    // }
+    //
+    // System.out.println("\nYou will be roleplaying in this game at a café.");
+    // System.out.println("Type the numbers next to the options to play.");
+    //
+    // while (l < 50) {
+    //   System.out.print(" - ");
+    //   Thread.sleep(100);
+    //   l++;
+    // }
+
+    System.out.println("\n\nWelcome to Café Cat Caca! What are you here for?\n\t1. A customer.\n\t2. The new employee.");
+    System.out.print("I am: ");
+
+    try {
+      String role = g.nextLine();
+      if ( role.equals("1") ) {
+        bob.cplay();
+      }
+      else if ( role.equals("2") ){
+        joe.eplay();
+      }
+    }
+    catch (Exception e) {
+      System.out.println("Something went wrong. Exiting game.");
+    }
+  }//end main
+}//end class
