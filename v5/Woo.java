11d40afe50d3fa3fff6a5d64ad1c86a8d41f1f20
jasonyang1119 <jyang30@stuy.edu>
2022-01-23 18:55:40 -0500

    test

--- v5/Woo.java
+++ v5/Woo.java
@@ -10,21 +10,21 @@ public class Woo {
     Employee joe = new Employee();
     System.out.println("\nLaunching Game...");
     //commenting out animationjust to save time for now whenever we run to fix errors
-    // int l = 0;
-    // while (l < 25) {
-    //   System.out.print(" - ");
-    //   Thread.sleep(100);
-    //   l++;
-    // }
-    //
-    // System.out.println("\nYou will be roleplaying in this game at a café.");
-    // System.out.println("Type the numbers next to the options to play.");
-    //
-    // while (l < 50) {
-    //   System.out.print(" - ");
-    //   Thread.sleep(100);
-    //   l++;
-    // }
+    int l = 0;
+    while (l < 25) {
+      System.out.print(" - ");
+      Thread.sleep(100);
+      l++;
+    }
+
+    System.out.println("\nYou will be roleplaying in this game at a café.");
+    System.out.println("Type the numbers next to the options to play.");
+
+    while (l < 50) {
+      System.out.print(" - ");
+      Thread.sleep(100);
+      l++;
+    }
 
     System.out.println("\n\nWelcome to Café Cat Caca! What are you here for?\n\t1. A customer.\n\t2. The new employee.");
     System.out.print("I am: ");
